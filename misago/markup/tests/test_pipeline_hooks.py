4d6964532ee585ec61081f7727965c308bdcedc6
swedge <scott.wedge@gmail.com>
2020-03-01 20:30:12 -0500

    Fixes #2

--- misago/markup/tests/test_pipeline_hooks.py
+++ misago/markup/tests/test_pipeline_hooks.py
@@ -7,11 +7,11 @@ def test_markdown_extensions_hook_is_called_by_pipeline(mocker):
     plugin = mocker.Mock()
     mocker.patch("misago.markup.pipeline.hooks.markdown_extensions", [plugin])
     pipeline.extend_markdown(mocker.Mock())
-    plugin.asssert_called_once_with(ANY)
+    plugin.assert_called_once_with(ANY)
 
 
 def test_parsing_result_processors_hook_is_called_by_pipeline(mocker):
     plugin = mocker.Mock()
     mocker.patch("misago.markup.pipeline.hooks.parsing_result_processors", [plugin])
     pipeline.extend_markdown(mocker.Mock())
-    plugin.asssert_called_once_with(ANY, ANY)
+    plugin.assert_called_once_with(ANY, ANY)
